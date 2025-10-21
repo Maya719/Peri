199dd97315a2dd64564a63c1322f39700dc24f0d
Maya719 <shoaib.mobipixels@gmail.com>
2025-10-21 16:19:38 +0500

    notepad

--- app/Filament/Client/Resources/TodoResource/Pages/EditTodo.php
+++ app/Filament/Client/Resources/TodoResource/Pages/EditTodo.php
@@ -9,4 +9,16 @@ use Filament\Resources\Pages\EditRecord;
 class EditTodo extends EditRecord
 {
     protected static string $resource = TodoResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
 }
